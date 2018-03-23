6cff6e834036de40df2175b0c66ac15f9976a605
designst <info@designstage.ch>
2018-03-23 18:35:40 +0100

    prepare generators

---
+++ scripts/generators/index.js
@@ -0,0 +1,31 @@
+/**
+ * generator/index.js
+ *
+ * Exports the generators so plop knows them
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const languageGenerator = require('./language');
+const componentGenerator = require('./component');
+const containerGenerator = require('./container');
+
+module.exports = plop => {
+  plop.setGenerator('component', componentGenerator);
+  plop.setGenerator('container', containerGenerator);
+  plop.setGenerator('language', languageGenerator);
+
+  plop.addHelper('directory', comp => {
+    try {
+      fs.accessSync(
+        path.join(__dirname, `../../app/containers/${comp}`),
+        fs.F_OK,
+      );
+      return `containers/${comp}`;
+    } catch (e) {
+      return `components/${comp}`;
+    }
+  });
+  plop.addHelper('curly', (object, open) => (open ? '{' : '}'));
+};
